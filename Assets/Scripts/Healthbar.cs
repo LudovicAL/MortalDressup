b1ae46e274454779acb90bf49cd85145b979da60
dturgeon85 <dturgeon85@gmail.com>
2018-12-01 15:37:38 -0500

    Health bar qui prend la valeur healthPoint d'un joueur.

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -5,27 +5,17 @@ using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour {
     private Image foreground;
-    private bool updateHealth;
-    public float waitTime = 30.0f;
-    public int playerHealth;
+    public GameObject go;
+    private Character character;
 
     // Use this for initialization
     void Start () {
         foreground = this.gameObject.GetComponent<Image>();
-        updateHealth = true;
-        playerHealth = 100;
+        character = go.GetComponent<Character>();
 	}
 
     // Update is called once per frame
     void Update() {
-
-        if (updateHealth == true)
-        {
-            //Reduce fill amount over 30 seconds
-            //foreground.fillAmount -= 1.0f / waitTime * Time.deltaTime;
-
-            foreground.fillAmount = 1.0f * playerHealth/100;
-        }
-
+        foreground.fillAmount = character.healthPoint/100.0f;
     }
 }
