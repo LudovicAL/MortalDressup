25a0a41ef5e833449986e527dcbda2089a027a42
dturgeon85 <dturgeon85@gmail.com>
2018-12-01 15:57:22 -0500

    Afficheage d'un countdown de 3 à 0 dans le debug.

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    float remainingTime;
+    int a;
+    string counter;
+
+    void Start()
+    {
+        remainingTime = 3;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        remainingTime -= Time.deltaTime;
+
+        a = (int)remainingTime;
+        counter = a.ToString();
+
+        print(counter);
+
+        if (remainingTime < 0)
+        {
+            print("0");
+        }
+    }
+}
