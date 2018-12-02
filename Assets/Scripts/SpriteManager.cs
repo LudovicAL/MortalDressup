4ee0be77bfa27eb1b6ea6536fc348793826122b0
dturgeon85 <dturgeon85@gmail.com>
2018-12-01 19:21:02 -0500

    Ajout du racter character chooser

--- Assets/Scripts/SpriteManager.cs
+++ Assets/Scripts/SpriteManager.cs
@@ -7,6 +7,7 @@ public class SpriteManager : MonoBehaviour
 {
     public static SpriteManager instance = null;     //Allows other scripts to call functions from SpriteManager.                    //Drag a reference to the sprite source which will display the sprite
     public Sprite[] sprites;
+    public Sprite[] characters;
 
 
     void Start()
@@ -30,6 +31,37 @@ public class SpriteManager : MonoBehaviour
         sprites[3] = Resources.Load<Sprite>("Sprites/WHAM");  
 
         this.sprites = sprites;
+    }
+
+    public void loadCharacters()
+    {
+
+        Sprite[] characters = new Sprite[12];
+        //Load an Sprite (Assets/Resources/Sprites/sprite01.png)
+
+        characters[0] = Resources.Load<Sprite>("Sprites/character_0");
+        characters[1] = Resources.Load<Sprite>("Sprites/character_1");
+        characters[2] = Resources.Load<Sprite>("Sprites/character_2");
+        characters[3] = Resources.Load<Sprite>("Sprites/character_3");
+        characters[4] = Resources.Load<Sprite>("Sprites/character_4");
+        characters[5] = Resources.Load<Sprite>("Sprites/character_5");
+        characters[6] = Resources.Load<Sprite>("Sprites/character_6");
+        characters[7] = Resources.Load<Sprite>("Sprites/character_7");
+        characters[8] = Resources.Load<Sprite>("Sprites/character_8");
+        characters[9] = Resources.Load<Sprite>("Sprites/character_9");
+        characters[10] = Resources.Load<Sprite>("Sprites/character_10");
+        characters[11] = Resources.Load<Sprite>("Sprites/character_11");
+
+        this.characters = characters;
+    }
+
+    public Sprite getRandomCharacter(params Sprite[] characters)
+    {
+        int randomIndex = Random.Range(0, characters.Length);
+        Debug.Log(characters.Length);
+        Debug.Log("Random" + randomIndex);
+        //Set the sprite to the sprite at our randomly chosen index.
+        return characters[randomIndex];
     }
 
     public Sprite getRandomFightingSprite()
